eabc42c260a8d9c6218b655a16282bc157922a69
Joel Tumambo <joeltumambs@gmail.com>
2019-06-19 12:35:42 +0800

    refactor(App.jsx): add borealis effect background

--- app/src/hocs/withLayout.js
+++ app/src/hocs/withLayout.js
@@ -7,10 +7,23 @@ const Body = styled.div`
     'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  background: linear-gradient(#283593, #5c6bc0);
+  background: linear-gradient(45deg, #9C27B0, #1A237E, #9C27B0, #FF7043);
+  background-size: 200% 200%;
   position: absolute;
   width: 100%;
   height: 100%;
+  animation: BorealisEffect 15s ease infinite;
+  @keyframes BorealisEffect {
+    0% {
+      background-position: 0% 50%
+    }
+    50% {
+      background-position: 100% 50%
+    }
+    100% {
+      background-position: 0% 50%
+    }
+  }
 `
 
 export default () => (WrappedComponent) => (props) => {
