5203dcc92c0fb15d9222cfcb704c337aba9ecab7
Nica Marielle Dela Cruz <niczdelacruz25@gmail.com>
2019-08-08 11:27:25 +0800

    fix: fixed profile screen UI

--- app/src/pages/Profile/index.jsx
+++ app/src/pages/Profile/index.jsx
@@ -1,31 +1,27 @@
 import React from 'react'
-import { compose, Query } from 'react-apollo'
+import { compose, Query, graphql } from 'react-apollo'
 import styled from 'styled-components'
-import { Button } from 'reactstrap'
+import { CardText } from 'reactstrap'
 import { withRouter } from 'react-router-dom'
+import uuid from 'uuid'
 
-import { FETCH_MY_TOPIC, FETCH_TAKEN_TOPIC } from './queries'
+import { FETCH_MY_TOPIC, FETCH_TAKEN_TOPIC, INSERT_USER_ACTIVITY } from './queries'
+import Button from '../../components/Button'
+import Icon from '../../components/Icon'
 
-const Author = styled.div`
-  color: #e8eaf6;
-  font-size: 12px;
-  line-height: 12px;
-  margin-bottom: 6px;
-`
-
-const AvatarContainer = styled.div`
-  border-radius: 50%;
-  width: 40%;
-  background-color: white;
-  height: 120px;
-`
+// const AvatarContainer = styled.div`
+//   border-radius: 50%;
+//   width: 40%;
+//   background-color: white;
+//   height: 120px;
+// `
 
 const ProfileInfo = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
-  padding-top: 20%;
   text-align: center;
+  font-size: 7em;
 `
 
 const CenteredText = styled.div`
@@ -38,76 +34,245 @@ const CenteredText = styled.div`
 const Container = styled.div`
   height: 100%;
   width: 100%;
+  margin: auto;
 `
 
-const Profile = ({ user, history }) => {
+const TopicWrapper = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  bottom: 80px;
+  width: 100%;
+  top: 20%;
+  padding: 40px;
+  padding-bottom: 0;
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 45%;
+`
+
+const SectionTitle = styled.div`
+  color: #c5cae9;
+  font-size: 12px;
+  margin-bottom: 4px;
+`
+
+const Title = styled.div`
+  color: #1a237e;
+  font-size: 20px;
+  line-height: 20px;
+  font-weight: 700;
+  max-height: 60%;
+  overflow-y: scroll;
+`
+
+const Author = styled.div`
+  color: #1a237e;
+  font-size: 12px;
+  opacity: 0.8;
+  line-height: 12px;
+  margin-bottom: 6px;
+`
+
+const Belt = styled.div`
+  position: absolute;
+  top: 6px;
+  bottom: 6px;
+  display: flex;
+`
+
+const TopicsContainer = styled.div`
+  position: relative;
+  overflow-x: scroll;
+  height: 100%;
+  margin-left: -40px;
+  margin-right: -40px;
+`
+
+const PreviewWrapper = styled.div`
+  background: linear-gradient(#e8eaf6, #c5cae9);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 0.5em;
+  width: 200px;
+  margin-left: 20px;
+  &:first-child {
+    margin-left: 40px;
+  }
+  &:last-child {
+    margin-right: 40px;
+  }
+  border-radius: 6px;
+  box-shadow: 0 6px 0 0 rgba(0, 0, 0, 0.2);
+  animation: Bounce cubic-bezier(0.445, 0.05, 0.55, 0.95) both 600ms;
+  animation-delay: ${({ n }) => n * 100 + 'ms'};
+`
+
+const Profile = ({ user, history, insertUserActivity }) => {
   console.log(user.id)
 
   return (
     <Container>
       <ProfileInfo>
-        <AvatarContainer />
+        {/* <AvatarContainer /> */}
+        <Icon name='perm_identity' />
       </ProfileInfo>
       <CenteredText>
         {user.first_name} {user.last_name}
       </CenteredText>
       <hr />
-      <CenteredText>Your created topics</CenteredText>
-      <Query query={FETCH_MY_TOPIC} variables={{ userId: user.id }}>
-        {({ data, error, loading }) => {
-          if (error) return <div>Error fetching topic: {error.message}</div>
-          if (loading) return <div>loading topic...</div>
-          const topics = data.topic
-          console.log('MY TOPICS: ', topics)
-          return (
-            <div style={{ display: 'flex', height: '100px', border: '2px solid black', overflowX: 'auto', overflowY: 'auto' }}>
-              {
-                topics.map(topic => {
-                /* eslint-disable camelcase */
-                  const date = new Date(topic.created_at)
-                  return (
-                    <div style={{ display: 'flex', flexDirection: 'column', height: '100px' }} key={topic.id} >
-                      <p>Topic: {topic.name}</p>
-                      <p>Desc: {topic.description}</p>
-                      <p>Date: {date.toDateString()}</p>
-                      <Author>upvote: {topic.ratings.length > 0 ? topic.ratings.filter(r => r.type === 'upvote').length : 0}</Author>
-                      <Author>downvote: {topic.ratings.length > 0 ? topic.ratings.filter(r => r.type === 'downvote').length : 0}</Author>
-                    </div>
-                  )
-                })
-              }
-            </div>
-          )
-        }}
-      </Query>
-      <br />
-      <CenteredText>
-        <Button
-          color='link'
-          onClick={() => {
-            history.push('/topic/create')
+      <TopicWrapper>
+        <Query query={FETCH_MY_TOPIC} variables={{ userId: user.id }}>
+          {({ data, error, loading }) => {
+            if (error) return <div>Error fetching topic: {error.message}</div>
+            if (loading) return <div>loading topic...</div>
+            const topics = data.topic
+            console.log('MY TOPICS: ', topics)
+            return (
+              <Wrapper>
+                <SectionTitle>Your created topics</SectionTitle>
+                <TopicsContainer>
+                  <Belt>
+                    {topics.length === 0 ? (
+                      <CenteredText>No Created Topics</CenteredText>
+                    ) : (
+                      topics.map((topic, index) => {
+                        const date = new Date(topic.created_at)
+                        return (
+                          <PreviewWrapper
+                            key={index}
+                            onClick={() => {
+                              if (user) {
+                                insertUserActivity({
+                                  variables: {
+                                    userActivity: {
+                                      id: uuid(),
+                                      activity_type: 'view',
+                                      user_id: user.id,
+                                      topic_id: topic.id
+                                    }
+                                  }
+                                })
+                                  .then((res) => {
+                                    console.log(res)
+                                  })
+                                  .catch((err) => {
+                                    console.log(err.message)
+                                  })
+                              }
+                              history.push(`topic/${topic.id}`)
+                            }}
+                          >
+                            <Title>{topic.name}</Title>
+                            <small className='text-muted'>Date: {date.toDateString()}</small>
+                            <CardText>{topic.description}</CardText>
+                            <Author>
+                              <Icon name='arrow_upward' />{' '}
+                              {topic.ratings.length > 0
+                                ? topic.ratings.filter((r) => r.type === 'upvote').length
+                                : 0}
+                            </Author>
+                            <Author>
+                              <Icon name='arrow_downward' />{' '}
+                              {topic.ratings.length > 0
+                                ? topic.ratings.filter((r) => r.type === 'downvote').length
+                                : 0}
+                            </Author>
+                          </PreviewWrapper>
+                        )
+                      })
+                    )}
+                  </Belt>
+                </TopicsContainer>
+              </Wrapper>
+            )
           }}
-        >
-          + create new topic
-        </Button>
-      </CenteredText>
-      <hr />
-      <CenteredText>Recent topics</CenteredText>
-      <Query query={FETCH_TAKEN_TOPIC} variables={{ userId: user.id }}>
-        {({ data, error, loading }) => {
-          if (error) return <div>Error fetching topic: {error.message}</div>
-          if (loading) return <div>loading topic...</div>
-          const topics = data.user_activity
-          console.log('TAKEN TOPICS:', topics)
-          return (
-            <div style={{ display: 'flex', height: '100px', border: '2px solid black', overflowX: 'auto', overflowY: 'auto' }} />
-          )
-        }}
-      </Query>
+        </Query>
+        <br />
+        <CenteredText>
+          <Button
+            text='Create New Topic'
+            type='primary'
+            onClick={() => {
+              history.push('/topic/create')
+            }}
+          />
+        </CenteredText>
+        <hr />
+        <Query query={FETCH_TAKEN_TOPIC} variables={{ userId: user.id }}>
+          {({ data, error, loading }) => {
+            if (error) return <div>Error fetching topic: {error.message}</div>
+            if (loading) return <div>loading topic...</div>
+            const topics = data.user_activity
+            console.log('TAKEN TOPICS:', topics)
+            return (
+              <Wrapper>
+                <SectionTitle>Recent Topics</SectionTitle>
+                <TopicsContainer>
+                  <Belt>
+                    {topics.length === 0 ? (
+                      <CenteredText>No Recent Topics</CenteredText>
+                    ) : (
+                      topics.map((topic, index) => {
+                        return (
+                          <PreviewWrapper
+                            key={index}
+                            onClick={() => {
+                              if (user) {
+                                insertUserActivity({
+                                  variables: {
+                                    userActivity: {
+                                      id: uuid(),
+                                      activity_type: 'view',
+                                      user_id: user.id,
+                                      topic_id: topic.topic.id
+                                    }
+                                  }
+                                })
+                                  .then((res) => {
+                                    console.log(res)
+                                  })
+                                  .catch((err) => {
+                                    console.log(err.message)
+                                  })
+                              }
+                              history.push(`topic/${topic.topic.id}`)
+                            }}
+                          >
+                            <Title>{topic.topic.name}</Title>
+                            <CardText>{topic.topic.description}</CardText>
+                            <Author>
+                              <Icon name='arrow_upward' />{' '}
+                              {topic.topic.ratings.length > 0
+                                ? topic.topic.ratings.filter((r) => r.type === 'upvote').length
+                                : 0}
+                            </Author>
+                            <Author>
+                              <Icon name='arrow_downward' />{' '}
+                              {topic.topic.ratings.length > 0
+                                ? topic.topic.ratings.filter((r) => r.type === 'downvote').length
+                                : 0}
+                            </Author>
+                          </PreviewWrapper>
+                        )
+                      })
+                    )}
+                  </Belt>
+                </TopicsContainer>
+              </Wrapper>
+            )
+          }}
+        </Query>
+      </TopicWrapper>
     </Container>
   )
 }
 
 export default compose(
-  withRouter
+  withRouter,
+  graphql(INSERT_USER_ACTIVITY, { name: 'insertUserActivity' })
 )(Profile)
--- app/src/pages/Profile/queries.js
+++ app/src/pages/Profile/queries.js
@@ -2,7 +2,7 @@ import gql from 'graphql-tag'
 
 export const FETCH_MY_TOPIC = gql`
   query fetchMyTopic($userId: uuid!) {
-    topic(order_by: {created_at: desc}, limit: 10, where: { creator_id: { _eq: $userId } }) {
+    topic(order_by: { created_at: desc }, limit: 10, where: { creator_id: { _eq: $userId } }) {
       id
       name
       description
@@ -16,7 +16,11 @@ export const FETCH_MY_TOPIC = gql`
 `
 export const FETCH_TAKEN_TOPIC = gql`
   query fetchTakenTopic($userId: uuid!) {
-    user_activity(order_by: {created_at: desc}, where: {user_id: {_eq: $userId}, activity_type: {_eq: "answer"}}, limit: 10) {
+    user_activity(
+      order_by: { created_at: desc }
+      where: { user_id: { _eq: $userId }, activity_type: { _eq: "answer" } }
+      limit: 10
+    ) {
       id
       topic {
         id
@@ -36,6 +40,13 @@ export const FETCH_TAKEN_TOPIC = gql`
           type
         }
       }
+    }
+  }
+`
+export const INSERT_USER_ACTIVITY = gql`
+  mutation insertUserActivity($userActivity: [user_activity_insert_input!]!) {
+    insert_user_activity(objects: $userActivity) {
+      affected_rows
     }
   }
 `
