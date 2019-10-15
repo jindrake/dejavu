7556b4566252bba21c4b4348027737bf604361c3
Pymy <pymycrosiacainglet@gmail.com>
2019-10-15 22:10:35 +0800

    refactor: fix analytics

--- app/src/pages/Analytics.jsx
+++ app/src/pages/Analytics.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { ListGroup, ListGroupItem, Button, Row, Col } from 'reactstrap'
-import { Title, ContentCenter, StyledInput, SubText, FullPageLoader } from '../components'
+import { ListGroup, Button, Col, Row } from 'reactstrap'
+import { Title, ContentCenter, StyledInput, SubText, FullPageLoader, DejavuCard } from '../components'
 import { useQuery } from '@apollo/react-hooks'
 import { FETCH_TOPIC } from './Topic/queries'
 import { useStateValue, getObjectValue } from '../libs'
@@ -76,55 +76,59 @@ const Analytics = ({
   const rankings = resultsData.get_topic_rankings ? JSON.parse(resultsData.get_topic_rankings) : []
   return (
     <Wrapper>
-      <div className='mb-3'>
-        <ContentCenter className='mt-5'>
-          <Title className='text-capitalize mb-5'>{topic.name} Analytics</Title>
-        </ContentCenter>
-        <ContentCenter>
-          <h1 className='display-4 font-weight-bold'>{analyticsData.get_topic_average_score}%</h1>
-        </ContentCenter>
-        <ContentCenter>
-          <SubText>Average Score</SubText>
-        </ContentCenter>
-        <div className='d-flex justify-content-around mt-3'>
-          <div className='p-10 col text-center mt-4 mb-3'>
-            <Title>{analyticsData.get_topic_takers_count}</Title>
-            <SubText>Users tackled the topic</SubText>
-          </div>
-          <div className='p-10 col text-center mt-4 mb-3'>
-            <Title>{analyticsData.solo_takers}</Title>
-            <SubText>Times tackled alone</SubText>
-          </div>
-          <div className='p-10 col text-center mt-4 mb-3'>
-            <Title>{analyticsData.duo_takers}</Title>
-            <SubText>Times tackled with a friend</SubText>
-          </div>
+      <Statistics className='justify-content-center'>
+        <div className='justify-content-center'>
+          <ContentCenter>
+            <Title className='text-capitalize mt-5 mb-4'>{topic.name} Analytics</Title>
+          </ContentCenter>
+          <ContentCenter>
+            <h1 className='display-4 font-weight-bold'>{analyticsData.get_topic_average_score}%</h1>
+          </ContentCenter>
+          <ContentCenter>
+            <SubText className='mb-3'>Average Score</SubText>
+          </ContentCenter>
         </div>
-        <div className='d-flex justify-content-around mt-3'>
-          <div className='p-10 col text-center mt-4 mb-3'>
-            <Title>{analyticsData.get_topic_comments_count}</Title>
-            <SubText>Comments</SubText>
-          </div>
-          <div className='p-10 col text-center mt-4 mb-3'>
-            <Title>
-              {topic.ratings.length > 0
-                ? topic.ratings.filter((r) => r.type === 'upvote').length
-                : 0}
-            </Title>
-            <SubText>Upvotes</SubText>
-          </div>
-          <div className='p-10 col text-center mt-4 mb-3'>
-            <Title>
-              {topic.ratings.length > 0
-                ? topic.ratings.filter((r) => r.type === 'downvote').length
-                : 0}
-            </Title>
-            <SubText>Downvotes</SubText>
-          </div>
+        <div className='justify-content-center'>
+          <Row className='justify-content-center m-3'>
+            <Col xs='4' sm='4' md='4' lg='4' xl='4' className='p-2 text-center text-wrap'>
+              <Title className='mb-2 dejavu-large-text'>{analyticsData.get_topic_takers_count}</Title>
+              <SubText className='dejavu-small-text'>Users tackled the topic</SubText>
+            </Col>
+            <Col xs='4' sm='4' md='4' lg='4' xl='4' className='p-2 text-center text-wrap'>
+              <Title className='mb-2 dejavu-large-text'>{analyticsData.solo_takers}</Title>
+              <SubText className='dejavu-small-text'>Times tackled alone</SubText>
+            </Col>
+            <Col xs='4' sm='4' md='4' lg='4' xl='4' className='p-2 text-center text-wrap'>
+              <Title className='mb-2 dejavu-large-text'>{analyticsData.duo_takers}</Title>
+              <SubText className='dejavu-small-text'>Times tackled with a friend</SubText>
+            </Col>
+          </Row>
+          <Row className='justify-content-center ml-3 mr-3 mt-3 pb-3'>
+            <Col xs='4' sm='4' md='4' lg='4' xl='4' className='p-2 text-center text-wrap'>
+              <Title className='mb-2 dejavu-large-text'>{analyticsData.get_topic_comments_count}</Title>
+              <SubText className='dejavu-small-text'>Comments</SubText>
+            </Col>
+            <Col xs='4' sm='4' md='4' lg='4' xl='4' className='p-2 text-center text-wrap'>
+              <Title className='mb-2 dejavu-large-text'>
+                {topic.ratings.length > 0
+                  ? topic.ratings.filter((r) => r.type === 'upvote').length
+                  : 0}
+              </Title>
+              <SubText className='dejavu-small-text'>Upvotes</SubText>
+            </Col>
+            <Col xs='4' sm='4' md='4' lg='4' xl='4' className='p-2 text-center text-wrap'>
+              <Title className='mb-2 dejavu-large-text'>
+                {topic.ratings.length > 0
+                  ? topic.ratings.filter((r) => r.type === 'downvote').length
+                  : 0}
+              </Title>
+              <SubText className='dejavu-small-text'>Downvotes</SubText>
+            </Col>
+          </Row>
         </div>
-      </div>
-      <Row className='p-3 bg-white'>
-        <ContentCenter className='mt-3'>
+      </Statistics>
+      <div className='overflow-auto p-3 bg-white'>
+        <ContentCenter className='mt-2'>
           <Title>Users</Title>
         </ContentCenter>
         <StyledInput
@@ -138,9 +142,9 @@ const Analytics = ({
           placeholder='Search user...'
         />
         <div className='m-3 d-inline'>
-          <SubText className='mr-2 d-inline'>Sort by:</SubText>
+          <SubText className='mr-2 d-inline dejavu-small-text'>Sort by:</SubText>
           <Button
-            className='m-1 border-0 rounded-pill text-wrap text-sm  bg-light text-dark'
+            className='dejavu-small-text m-1 border-0 rounded-pill text-wrap text-sm  bg-light'
             active={sortBy === 'average'}
             onClick={() => {
               setSortBy('average')
@@ -149,7 +153,7 @@ const Analytics = ({
             Rank
           </Button>
           <Button
-            className='m-1 rounded-pill text-wrap text-sm border-0 bg-light text-dark'
+            className='dejavu-small-text m-1 rounded-pill text-wrap text-sm border-0 bg-light'
             active={sortBy === 'name'}
             onClick={() => {
               setSortBy('name')
@@ -158,7 +162,7 @@ const Analytics = ({
             Name
           </Button>
           <Button
-            className='m-1 rounded-pill text-wrap text-sm border-0 bg-light text-dark'
+            className='dejavu-small-text m-1 rounded-pill text-wrap text-sm border-0 bg-light'
             active={sortBy === 'coverage'}
             onClick={() => {
               setSortBy('coverage')
@@ -167,58 +171,58 @@ const Analytics = ({
             Items Answered
           </Button>
         </div>
-        <ResultsWrapper>
+        <div>
           {rankings
             .filter(
               ({ userInfo: { first_name: firstName, last_name: lastName } }) =>
                 firstName.includes(search) || lastName.includes(search)
             )
             .map((rankingData, index) => {
+              console.log('thernkingdata', rankingData)
               return (
                 <ListGroup key={index}>
-                  <ListGroupItem>
+                  <DejavuCard>
                     <Row>
-                      <Col className='justify-content-between' xs='9'>
+                      <Col xs='8' sm='8' md='8' lg='8' xl='8' className='justify-content-between'>
                         <Title className='m-0 text-capitalize'>
                           {rankingData.userInfo.first_name} {rankingData.userInfo.last_name}
                         </Title>
                         <div className='m-0'>
-                          <SubText>{rankingData.questionIdsSeen.length} items answered</SubText>
-                          <SubText>out of {topic.questions.length}</SubText>
+                          <SubText className='dejavu-small-text'>{rankingData.questionIdsSeen.length} items answered</SubText>
+                          <SubText className='dejavu-small-text'>out of {topic.questions.length}</SubText>
                         </div>
                       </Col>
-                      <Col className='text-center' xs='3'>
-                        <Title className='font-weight-bold m-0'>
+                      <Col xs='4' sm='4' md='4' lg='4' xl='4' className='text-center'>
+                        <Title className='text-capitalize font-weight-bold m-0'>
                           {Number(
                             (rankingData.numberOfCorrect / rankingData.totalTimesAnswered) * 100
                           ).toFixed(2)}
                         </Title>
-                        <SubText className='m-0'>Score</SubText>
+                        <SubText className='dejavu-small-text m-0'>Score</SubText>
                       </Col>
                     </Row>
-                  </ListGroupItem>
+                  </DejavuCard>
                 </ListGroup>
               )
             })}
-        </ResultsWrapper>
-      </Row>
+        </div>
+      </div>
     </Wrapper>
   )
 }
 
-const ResultsWrapper = styled.div`
-  margin-bottom: 10px;
-  overflow: auto;
-  display: flex;
-  flex-direction: column;
-  height: 70%;
+const Statistics = styled.div`
+  background-color: #5eb2fb;
   width: 100%;
+  color: white;
 `
 
 const Wrapper = styled.div`
-  width: 100%;
   height: 100%;
-  color: black !important;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
 `
 
 export default Analytics
